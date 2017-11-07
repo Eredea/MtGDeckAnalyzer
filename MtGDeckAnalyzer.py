f2e1f76a52f8e1d12d0eb68179b6076ad92cbce9
Sean Meredith <Eredea@localhost.localdomain>
2017-11-07 13:29:39 -0500

    Saving

--- MtGDeckAnalyzer.py
+++ MtGDeckAnalyzer.py
@@ -11,8 +11,10 @@ from matplotlib.figure import Figure
 
 
 default_decklist = """Llanowar elves
-Dark Ritual
+Brainstorm
 Swords to plowshares
+Lightning Storm
+Drain Life
 """
 
 # May want to later add functionality here, so abstracted it out
@@ -29,56 +31,6 @@ class Client:
         self.ui.start_UI()
 
     #Not even used in the app yet
-    cardInfo = """
-    Name: {}
-    Set: {}
-    Rarity: {}
-    Card Text: {}
-    """
-
-
-# Here are custom tk widgets used in the MainPage class
-class CardListDisplay(tkinter.Listbox):
-    """Custom tkinter Listbox widget which automatically binds the click-list event."""
-    def __init__(self, masterFrame, linkedDisplay = False, deck = Deck(), **kwargs, ):
-        # Under this super call I'm wondering the differences if I refer to self.masterFrame or not
-        # I don't think there's a difference here.
-        self.masterFrame = masterFrame
-        super().__init__(self.masterFrame, kwargs)
-
-        self.deck = deck
-        self.linkedDisplay = linkedDisplay
-        if self.linkedDisplay:
-            self.bind('<<ListboxSelect>>', self.click_deck_list)
-
-    def add_card(self, card):
-        card = card if type(card) is Card else get_card_by_name(card)
-        self.insert('end',card.name)
-        self.deck.add_card(card)
-
-
-    def show_deck(self, deck):
-        """Updates listbox UI with card names from deck object."""
-        self.deck = deck
-        self.delete(0,'end')
-        for card in deck:
-            self.insert('end', card.name)
-
-
-    def click_deck_list(self, evt):
-        w = evt.widget
-        if w.curselection():
-            index = int(w.curselection()[0])
-            cardName = w.get(index)
-            card = self.deck.get_card(cardName)
-            print(card)
-            print(card.name)
-            self.masterFrame.display_card(card)
-
-    @property
-    def selectedCard(self):
-        pass
-
 
 class MainPage:
 
@@ -91,58 +43,68 @@ class MainPage:
         self.master.display_card = self.display_card
 
 
-        self.leftDisplay = LeftDisplay(self.master,
-                                       bg= 'black',
-                                       width = 50,
-                                       borderwidth = 1,
-                                       relief = 'raised')
+        self.leftDisplay = LeftDisplay(self.master,bg= 'black',width = 50,borderwidth = 1,relief = 'raised')
         self.leftDisplay.deckListButton['command'] = lambda: self.add_to_deck_list(self.leftDisplay.displayedCard)
+        self.leftDisplay.cardPoolButton['command'] = lambda: self.add_to_card_pool(self.leftDisplay.displayedCard)
 
         # This is the card pool display spanning the bottom of the screen.
-        self.deckListDisplay = CardListDisplay(self.master, linkedDisplay=True, width =100, height =20 )
-        self.deckListDisplay.show_deck(deck)
+        self.cardPoolDisplay = CardListDisplay(self.master, linkedDisplay=True, width =100, height =20)
 
         self.stats_viewer = StatisticViewer(self.master)
         self.statsButton = tkinter.Button(self.master, command= lambda: self.stats_viewer.refreshFigure(self.deck.manaColorProportions))
 
+        self.deckListDisplay = CardListDisplay(self.master, linkedDisplay=True, height = 40, width = 30)
+        self.deckListDisplay.show_deck(deck)
 
         self.leftDisplay.pack(fill='y', side='left')
-        self.deckListDisplay.pack(fill='both',side='bottom')
+        self.cardPoolDisplay.pack(fill='both', side='bottom')
+        self.deckListDisplay.pack(fill = 'both', side = 'right')
         self.stats_viewer.pack()
         self.statsButton.pack()
+        tkinter.Label(text = "Your card pool:").pack()
 
     def start_UI(self):
         self.master.mainloop()
 
     def display_card(self, card):
+        # For now just uses left's display, but we could add functionality later
         self.leftDisplay.display_card(card)
 
+    def add_to_card_pool(self,card):
+        self.cardPoolDisplay.add_card(card)
 
     def add_to_deck_list(self, card):
             self.deckListDisplay.add_card(card)
             self.deck.add_card(card)
 
-#Here are individual frame objects that make up the main frame:
+#Here are individual frame objects that make up the mainpage "master" frame:
 class LeftDisplay(tkinter.Frame):
+    cardInfo = """
+    Name: {}
+    Set: {}
+    Rarity: {}
+    Card Text: {}
+    """
 
     def __init__(self, parent, **kwargs):
         """Creates and configures widgets that will be placed on the left side of the screen.
             Packing is done at the end."""
         super().__init__(parent, kwargs)
+        self.parent = parent
 
         # There is always one displayed card in this area, and functions act around it.
         self.displayedCard = get_card_by_name("Khalni Hydra")
 
         # Placeholder image object
-        self.image = Image.open("Khalni Hydra.png")
-        self.photo = ImageTk.PhotoImage(self.image)
+        self.image = get_card_picture(self.displayedCard)
 
         # The display of the current card is created and placed.
-        self.cardImageBox = tkinter.Label(self, bg='black',image=self.photo, height=300, width=300)
-        self.cardImageBox.image = self.photo
+        self.cardImageBox = tkinter.Label(self, bg='black',image=self.image, height=300, width=300)
+        self.cardImageBox.image = self.image
 
         #Here we create the text that displays MtG information about the card
-        self.cardInfoDisplay = tkinter.Label(self, text="Card Information will be displayed here.", height = 5, width = 50, )
+
+        self.cardInfoDisplay = tkinter.Message(self, text="Card Information will be displayed here.", justify='left')
 
         #Searchbox for cards and corresponding results
         self.cardSearchBox = tkinter.Entry(self)
@@ -151,36 +113,74 @@ class LeftDisplay(tkinter.Frame):
 
         self.searchButton = tkinter.Button(self, command=lambda: self.search_card(self.cardSearchBox.get()),text="Search for Card")
 
-        # Here is a design point
+        # Here is a design point maybe
         # When the button is clicked, we need to both alter the display and update the APPLICATION's current deck values
         # We could from the MYUI class do self.leftdisplay.deckListButton[command] from a place where we have more scope
+        # But now our code is more seperated.
         self.deckListButton = tkinter.Button(self, font = 'Times', bg='black', fg= 'white', text="Add to DeckList")
+        self.cardPoolButton = tkinter.Button(self, font = 'Times', bg='black', fg= 'white', text="Add to Cardpool")
+        self.advancedSearchButton = tkinter.Button(self, text = "Bring advanced search", command = self.advanced_search)
+
 
         self.cardImageBox.pack(side='top')
-        self.cardInfoDisplay.pack(fill='y')
+        self.cardInfoDisplay.pack(fill='both')
         self.searchResultsList.pack(fill='both')
         self.cardSearchBox.pack()
         self.searchButton.pack()
         self.deckListButton.pack()
+        self.cardPoolButton.pack()
+        self.advancedSearchButton.pack()
+
+    def advanced_search(self):
+        window = tkinter.Toplevel(self.parent)
+
+        horizontal = tkinter.Frame(window)
+
+        frameObj = tkinter.Frame(window, height = 300, width = 300)
+
+        choices = ['AND','OR','NOT']
+        variable = tkinter.StringVar(window)
+        variable.set("AND")
+        andOrBox = tkinter.OptionMenu(horizontal, variable, *choices)
+        andOrBox.pack(side = 'left', )
+
+
+        searchBox = tkinter.Entry(horizontal)
+        searchBox.pack(side = 'left')
+
+        resultBox = CardListDisplay(window)
+
+        searchButton = tkinter.Button(window, command = lambda: adv_search_card(searchBox.get()))
+
+        searchButton.pack()
+        horizontal.pack()
+        resultBox.pack()
+        frameObj.pack()
+
+
+        def adv_search_card(cardName):
+            results = Deck.search(cardName)
+            resultBox.show_deck(results)
 
     def display_card(self, card):
 
         if type(card) is str:
-            print("Used str")
+            print("Used str, should almost never happen")
             card = get_card_by_name(card)
         print("card for display_card", card)
         image = get_card_picture(card)
         self.cardImageBox.config(image = image)
         # This is necessary because tkinter doesn't update python, so we need to hold on to the reference
         self.cardImageBox.image = image
-        self.cardInfoDisplay.config(text = card.set_name + " " + card.text)
+        # We could later just implement a card.displayInfo property, but extending card is hard because of how they're built. Will have to change module
+        self.cardInfoDisplay.config(text = card.set_name + " " + card.text if card.text else card.set_name)
         self.displayedCard = card
 
     # Callback for searchButton
     def search_card(self, cardName):
         # When Searching I could populate all results and have them stored ready to click
         # Right now I just display the search results and then when clicking the listbox create another card item
-        searchResults = Deck.search(cardName)
+        searchResults = search(cardName)
         self.searchResultsList.show_deck(searchResults)
 
 class StatisticViewer(tkinter.Frame):
@@ -213,6 +213,56 @@ class StatisticViewer(tkinter.Frame):
         self.canvas.draw()
 
     def reAnalyze(self, deck):
+        pass
+
+# Here are custom tk widgets used in the MainPage class
+class CardListDisplay(tkinter.Listbox):
+    """Custom tkinter Listbox widget which automatically binds the click-list event."""
+    def __init__(self, masterFrame, linkedDisplay = False, deck = Deck(), **kwargs, ):
+        self.masterFrame = masterFrame
+        super().__init__(self.masterFrame, kwargs)
+
+        self.deck = deck
+        self.linkedDisplay = linkedDisplay
+        if self.linkedDisplay:
+            self.bind('<<ListboxSelect>>', self.click_deck_list)
+        self.bind('<Double-1>', self.doubleclick)
+    def add_card(self, card):
+        card = card if type(card) is Card else get_card_by_name(card)
+        self.insert('end',card.name)
+        self.deck.add_card(card)
+
+    def doubleclick(self, event):
+        w = event.widget
+        if w.curselection():
+            index = int(w.curselection()[0])
+            card = self.deck.get_card(w.get(index))
+            image = get_card_picture(card)
+        window = tkinter.Toplevel(self.masterFrame)
+        picture = tkinter.Label(window, image = image)
+        picture.pack()
+        picture.image = image
+
+    def show_deck(self, deck):
+        """Updates listbox UI with card names from deck object."""
+        self.deck = deck
+        self.delete(0,'end')
+        for card in deck:
+            self.insert('end', card.name)
+
+
+    def click_deck_list(self, evt):
+        w = evt.widget
+        if w.curselection():
+            index = int(w.curselection()[0])
+            cardName = w.get(index)
+            card = self.deck.get_card(cardName)
+            print(card)
+            print(card.name)
+            self.masterFrame.display_card(card)
+
+    @property
+    def selectedCard(self):
         pass
 
 
--- MtGUtils.py
+++ MtGUtils.py
@@ -1,12 +1,5 @@
 from mtgsdk import Card
-from mtgsdk import Set
-from mtgsdk import Type
-from mtgsdk import Supertype
-from mtgsdk import Subtype
-from mtgsdk import Changelog
-import wget
-from PIL import Image, ImageTk
-
+import collections
 
 def get_card_by_name(name):
     """ This takes in a card name, and returns a single card object.
@@ -15,16 +8,42 @@ def get_card_by_name(name):
         :param name: Name of the card to search
         :returns: Card object
         """
-    return next(card for card in Card.where(name = name).all() if card.image_url is not None)
+    return next(card for card in Card.where(name = name) if card.image_url is not None)
+
+def search(cardStr):
+    """This is to be used to search the api for a card by ambiguous parameters and return a deck container with the resulting cards
+        I think I am going to update it to contain a dictionary that maps the keys and values to the filters used."""
+    results = Card.where(name=cardStr, ).all()
+    cardNames = set(card.name for card in results)
+    cardList = [next(card for card in results if card.name == name and card.image_url is not None) for name in cardNames]
+
+    deck = Deck(cardList)
+    return deck
 
 
-class Deck:
+
+
+
+
+    """
+    for cardname in cardNames:
+        for card in results:
+            if card.name == cardname and card.image_url is not None:
+                deck.add_card(card)
+                break
+            continue
+
+            """
+
+
+
+class Deck(collections.Iterable):
 
     # Colors of the cards don't change, so we make this a class variable
     color_ids = 'B', 'U', 'G', 'R', 'W', None
 
-    def __init__(self, cards = () ):
-        self.cards = [get_card_by_name(card) for card in cards]
+    def __init__(self, cards = [] ):
+        self.cards = [get_card_by_name(card) for card in cards] if cards and type(cards[0]) is str else cards
 
     def __iter__(self):
         for card in self.cards:
@@ -33,17 +52,23 @@ class Deck:
     def __str__(self):
         return '\n'.join([card.name for card in self])
 
+
     @property
     def mean_mana(self):
         return sum([card.cmc for card in self.cards])/len(self.cards)
 
     @property
     def mana_curve_tuple(self):
+        max = max()
         return []
 
     @property
     def manaColorProportions(self):
-        colors = [card.color_identity[0] for card in self.cards]
+        for card in self.cards:
+            print(card.mana_cost)
+
+        #I'm sure this area could be chopped down
+        colors = [card.color_identity[0] if type(card.color_identity) is type([]) else None for card in self.cards]
         counts = [colors.count(id) for id in Deck.color_ids]
         proportions = [count/len(colors) for count in counts]
         return proportions
@@ -62,32 +87,13 @@ class Deck:
     def average_creature_power(self):
         pass
 
-    def get_card(self, cardName):
-        return next(card for card in self.cards if card.name == cardName)
+    def get_card(self, cardNameStr):
+        return next(card for card in self.cards if card.name == cardNameStr)
 
     def add_card(self, card):
         """Should be called with a string or a Card object"""
         #self.cards.append(card) if type(card) == Card else self.cards.append(get_card_by_name(card))
         self.cards.append(card) if type(card) is Card else self.cards.append(get_card_by_name(card))
-
-    @staticmethod
-    def search(cardStr):
-        #TODO optimize this because I am creating card objects twice
-        #I think it may be better optimized now but it's ugly as all hell
-        results = Card.where(name=cardStr).all()
-        cardNames = set(card.name for card in results)
-        deck = Deck()
-        for cardname in cardNames:
-            for card in results:
-                if card.name == cardname and card.image_url is not None:
-                    deck.add_card(card)
-                    break
-                continue
-        return deck
-
-
-
-
 
 
 
